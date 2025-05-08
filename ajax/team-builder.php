59e11500de0098e8b873fb32321af78a3923d5c9
MoxoPixel <hello@emils.graphics>
2025-05-08 17:55:03 +0200

    Fixed bug on grid design for team builder

--- ajax/team-builder.php
+++ ajax/team-builder.php
@@ -56,107 +56,105 @@ $teamStatsAdv = $activeTeam ? getTeamStatsAdv($activeTeam, $season) : null;
     </div>
 </div>
 
-<main>
-    <div class="wrap">
-        <div class="tb-mobile-alert alert">
-            <div class="alert-icon"><i class="bi bi-exclamation-triangle"></i></div>
-            <div class="alert-text">This feature is not made to be used with mobile devices.</div>
-        </div>
-        <div class="team-roster team-builder">
+<div class="wrap">
+    <div class="tb-mobile-alert alert">
+        <div class="alert-icon"><i class="bi bi-exclamation-triangle"></i></div>
+        <div class="alert-text">This feature is not made to be used with mobile devices.</div>
+    </div>
+    <div class="team-roster team-builder">
 
-            <div class="select-players-button component-header">
-                <h3 class="title">Team Builder</h3>
-                <div class="flex-default">
-                    <div class="custom-select">
-                        <input class="dropdown" type="checkbox" id="dropdownBuilder" name="dropdownBuilder">
-                        <label class="for-dropdown" for="dropdownBuilder">Select Team <i class="bi bi-arrow-down-short"></i></label>
-                        <div class="section-dropdown" id="team-selection-custom"> 
-                            <div class="fader-top"></div>
-                            <div class="container">
-                                <?php include '../includes/teamSelection.php'; ?>
-                            </div>
-                            <div class="fader-bottom"></div>
+        <div class="select-players-button component-header">
+            <h3 class="title">Team Builder</h3>
+            <div class="flex-default">
+                <div class="custom-select">
+                    <input class="dropdown" type="checkbox" id="dropdownBuilder" name="dropdownBuilder">
+                    <label class="for-dropdown" for="dropdownBuilder">Select Team <i class="bi bi-arrow-down-short"></i></label>
+                    <div class="section-dropdown" id="team-selection-custom"> 
+                        <div class="fader-top"></div>
+                        <div class="container">
+                            <?php include '../includes/teamSelection.php'; ?>
                         </div>
+                        <div class="fader-bottom"></div>
                     </div>
-                    <button popovertarget="team-builder-player-pool" class="btn sm disabled"><i class="bi bi-person-plus"></i> Add</button>
-                    <button class="btn sm" id="btn-clear-tb"><i class="bi bi-trash"></i> Clear</button>
+                </div>
+                <button popovertarget="team-builder-player-pool" class="btn sm disabled"><i class="bi bi-person-plus"></i> Add</button>
+                <button class="btn sm" id="btn-clear-tb"><i class="bi bi-trash"></i> Clear</button>
+            </div>
+        </div>
+
+        <div class="team-lines" id="team-builder-drop-area">
+            <div class="line-group forwards-lines">
+                <h3>Forward Lines</h3>
+                <div class="line">
+                    <div class="line-label">Line 1</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                    </div>
+                </div>
+                <div class="line">
+                    <div class="line-label">Line 2</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                    </div>
+                </div>
+                <div class="line">
+                    <div class="line-label">Line 3</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                    </div>
+                </div>
+                <div class="line">
+                    <div class="line-label">Line 4</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                        <div class="player-slot" data-position="forward"></div>
+                    </div>
                 </div>
             </div>
 
-            <div class="team-lines" id="team-builder-drop-area">
-                <div class="line-group forwards-lines">
-                    <h3>Forward Lines</h3>
-                    <div class="line">
-                        <div class="line-label">Line 1</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                        </div>
+            <div class="line-group defense-lines">
+                <h3>Defense Pairing</h3>
+                <div class="line">
+                    <div class="line-label">Pair 1</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="defenseman"></div>
+                        <div class="player-slot" data-position="defenseman"></div>
                     </div>
-                    <div class="line">
-                        <div class="line-label">Line 2</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                        </div>
+                </div>
+                <div class="line">
+                    <div class="line-label">Pair 2</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="defenseman"></div>
+                        <div class="player-slot" data-position="defenseman"></div>
                     </div>
-                    <div class="line">
-                        <div class="line-label">Line 3</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                        </div>
-                    </div>
-                    <div class="line">
-                        <div class="line-label">Line 4</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                            <div class="player-slot" data-position="forward"></div>
-                        </div>
+                </div>
+                <div class="line">
+                    <div class="line-label">Pair 3</div>
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="defenseman"></div>
+                        <div class="player-slot" data-position="defenseman"></div>
                     </div>
                 </div>
 
-                <div class="line-group defense-lines">
-                    <h3>Defense Pairing</h3>
-                    <div class="line">
-                        <div class="line-label">Pair 1</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="defenseman"></div>
-                            <div class="player-slot" data-position="defenseman"></div>
-                        </div>
-                    </div>
-                    <div class="line">
-                        <div class="line-label">Pair 2</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="defenseman"></div>
-                            <div class="player-slot" data-position="defenseman"></div>
-                        </div>
-                    </div>
-                    <div class="line">
-                        <div class="line-label">Pair 3</div>
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="defenseman"></div>
-                            <div class="player-slot" data-position="defenseman"></div>
-                        </div>
-                    </div>
-
-                    <div class="line-group goalie-lines">
-                    <h3>Starting Goalies</h3>
-                    <div class="line">
-                        <div class="line-slots">
-                            <div class="player-slot" data-position="goalie"></div>
-                            <div class="player-slot" data-position="goalie"></div>
-                        </div>
+                <div class="line-group goalie-lines">
+                <h3>Starting Goalies</h3>
+                <div class="line">
+                    <div class="line-slots">
+                        <div class="player-slot" data-position="goalie"></div>
+                        <div class="player-slot" data-position="goalie"></div>
                     </div>
                 </div>
-                </div>
+            </div>
             </div>
         </div>
     </div>
-</main>
+</div>
 
 <?php if(isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {} else { include_once '../footer.php'; } ?>
