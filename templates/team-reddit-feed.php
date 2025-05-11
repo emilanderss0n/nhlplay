9593aedcf4f02ceafc5c87f959f8c7739f3fa528
MoxoPixel <hello@emils.graphics>
2025-05-11 22:43:43 +0200

    Added more specific more button

--- templates/team-reddit-feed.php
+++ templates/team-reddit-feed.php
@@ -7,7 +7,7 @@ if (!isset($teamSubreddit) || empty($teamSubreddit)) {
 
 <div class="component-header" style="margin-top: 3rem">
     <h3 class="title">r/<?= $teamSubreddit ?> - Recent Posts</h3>
-    <a href="https://www.reddit.com/r/<?= $teamSubreddit ?>/" target="_blank" rel="noopener noreferrer" class="btn sm">View More</a>
+    <a href="https://www.reddit.com/r/<?= $teamSubreddit ?>/" target="_blank" rel="noopener noreferrer" class="btn sm">View r/<?= $teamSubreddit ?></a>
 </div>
 <div class="reddit-feed" id="team-reddit-feed-section" data-subreddit="<?= $teamSubreddit ?>" data-limit="8">
     <div class="reddit-posts grid grid-300 grid-gap grid-gap-row">
