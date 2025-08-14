9901fe8d2357854405f02ce3d02e6890da767512
MoxoPixel <hello@emils.graphics>
2025-08-14 14:45:13 +0200

    Added post season notice on recaps page

--- ajax/recaps.php
+++ ajax/recaps.php
@@ -5,17 +5,27 @@ if(isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && $_SERVER['HTTP_X_REQUESTED_WITH
 ?>
 <main>
     <div class="wrap">
+        <?php if (!$seasonBreak) {
+        $startDate = date('Y-m-d', strtotime('-4 day'));
+        // Use the new NHL API utility
+        $ApiUrl = NHLApi::scheduleByDate($startDate);
+        $curl = curlInit($ApiUrl);
+        $schedules = json_decode($curl);
+        ?>
         <div class="game-recaps schedule grid grid-300 grid-gap-lg grid-gap-row-sm" grid-max-col-count="4">
-            <div class="alert">No recaps available, try older games below</div>
-            <?php 
-            $startDate = date('Y-m-d', strtotime('-4 day'));
-            // Use the new NHL API utility
-            $ApiUrl = NHLApi::scheduleByDate($startDate);
-            $curl = curlInit($ApiUrl);
-            $schedules = json_decode($curl);
-            gameRecaps($schedules);
-           ?>
+        <?php gameRecaps($schedules);  ?>
         </div>
+        <?php } if ($seasonBreak) { ?>
+        <div class="game-recaps">
+            <div class="season-break-notice">
+                <img src="assets/img/post-season-2.png" alt="Season Break" />
+                <div>
+                    <h3>It's post season baby!</h3>
+                    <span>Meanwhile, check out <a href="last-season-overview" rel="page">last season overview</a> to see how teams and players performed</span>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
     </div>
 </main>
 <?php if(isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {} else { include_once '../footer.php'; } ?>
