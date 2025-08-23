e6ebf6025cb3f2a18fc1162b61df5fc36c94ff27
MoxoPixel <hello@emils.graphics>
2025-08-23 02:10:10 +0200

    Enhance team selection UI with improved dropdown styling and visibility adjustments

--- includes/teamSelection.php
+++ includes/teamSelection.php
@@ -1,32 +1,32 @@
-<a href="javascript:void(0);" data-value="24" data-abbr="ANA"><span><img src="assets/img/teams/png/24.png" class="team-img" alt="Anaheim Ducks" /></span>Anaheim Ducks</a>
-<a href="javascript:void(0);" data-value="6" data-abbr="BOS"><span><img src="assets/img/teams/png/6.png" class="team-img" alt="Boston Bruins" /></span>Boston Bruins</a>
-<a href="javascript:void(0);" data-value="7" data-abbr="BUF"><span><img src="assets/img/teams/png/7.png" class="team-img" alt="Buffalo Sabres" /></span>Buffalo Sabres</a>
-<a href="javascript:void(0);" data-value="20" data-abbr="CGY"><span><img src="assets/img/teams/png/20.png" class="team-img" alt="Calgary Flames" /></span>Calgary Flames</a>
-<a href="javascript:void(0);" data-value="12" data-abbr="CAR"><span><img src="assets/img/teams/png/12.png" class="team-img" alt="Carolina Hurricanes" /></span>Carolina Hurricanes</a>
-<a href="javascript:void(0);" data-value="16" data-abbr="CHI"><span><img src="assets/img/teams/png/16.png" class="team-img" alt="Chicago Blackhawks" /></span>Chicago Blackhawks</a>
-<a href="javascript:void(0);" data-value="21" data-abbr="COL"><span><img src="assets/img/teams/png/21.png" class="team-img" alt="Colorado Avalanche" /></span>Colorado Avalanche</a>
-<a href="javascript:void(0);" data-value="29" data-abbr="CBJ"><span><img src="assets/img/teams/png/29.png" class="team-img" alt="Columbus Blue Jackets" /></span>Columbus Blue Jackets</a>
-<a href="javascript:void(0);" data-value="25" data-abbr="DAL"><span><img src="assets/img/teams/png/25.png" class="team-img" alt="Dallas Stars" /></span>Dallas Stars</a>
-<a href="javascript:void(0);" data-value="17" data-abbr="DET"><span><img src="assets/img/teams/png/17.png" class="team-img" alt="Detroit Red Wings" /></span>Detroit Red Wings</a>
-<a href="javascript:void(0);" data-value="22" data-abbr="EDM"><span><img src="assets/img/teams/png/22.png" class="team-img" alt="Edmonton Oilers" /></span>Edmonton Oilers</a>
-<a href="javascript:void(0);" data-value="13" data-abbr="FLA"><span><img src="assets/img/teams/png/13.png" class="team-img" alt="Florida Panthers" /></span>Florida Panthers</a>
-<a href="javascript:void(0);" data-value="26" data-abbr="LAK"><span><img src="assets/img/teams/png/26.png" class="team-img" alt="Los Angeles Kings" /></span>Los Angeles Kings</a>
-<a href="javascript:void(0);" data-value="30" data-abbr="MIN"><span><img src="assets/img/teams/png/30.png" class="team-img" alt="Minnesota Wild" /></span>Minnesota Wild</a>
-<a href="javascript:void(0);" data-value="8" data-abbr="MTL"><span><img src="assets/img/teams/png/8.png" class="team-img" alt="Montréal Canadiens" /></span>Montréal Canadiens</a>
-<a href="javascript:void(0);" data-value="18" data-abbr="NSH"><span><img src="assets/img/teams/png/18.png" class="team-img" alt="Nashville Predators" /></span>Nashville Predators</a>
-<a href="javascript:void(0);" data-value="1" data-abbr="NJD"><span><img src="assets/img/teams/png/1.png" class="team-img" alt="New Jersey Devils" /></span>New Jersey Devils</a>
-<a href="javascript:void(0);" data-value="2" data-abbr="NYI"><span><img src="assets/img/teams/png/2.png" class="team-img" alt="New York Islanders" /></span>New York Islanders</a>
-<a href="javascript:void(0);" data-value="3" data-abbr="NYR"><span><img src="assets/img/teams/png/3.png" class="team-img" alt="New York Rangers" /></span>New York Rangers</a>
-<a href="javascript:void(0);" data-value="9" data-abbr="OTT"><span><img src="assets/img/teams/png/9.png" class="team-img" alt="Ottawa Senators" /></span>Ottawa Senators</a>
-<a href="javascript:void(0);" data-value="4" data-abbr="PHI"><span><img src="assets/img/teams/png/4.png" class="team-img" alt="Philadelphia Flyers" /></span>Philadelphia Flyers</a>
-<a href="javascript:void(0);" data-value="5" data-abbr="PIT"><span><img src="assets/img/teams/png/5.png" class="team-img" alt="Pittsburgh Penguins" /></span>Pittsburgh Penguins</a>
-<a href="javascript:void(0);" data-value="28" data-abbr="SJS"><span><img src="assets/img/teams/png/28.png" class="team-img" alt="San Jose Sharks" /></span>San Jose Sharks</a>
-<a href="javascript:void(0);" data-value="55" data-abbr="SEA"><span><img src="assets/img/teams/png/55.png" class="team-img" alt="Seattle Kraken" /></span>Seattle Kraken</a>
-<a href="javascript:void(0);" data-value="19" data-abbr="STL"><span><img src="assets/img/teams/png/19.png" class="team-img" alt="St. Louis Blues" /></span>St. Louis Blues</a>
-<a href="javascript:void(0);" data-value="14" data-abbr="TBL"><span><img src="assets/img/teams/png/14.png" class="team-img" alt="Tampa Bay Lightning" /></span>Tampa Bay Lightning</a>
-<a href="javascript:void(0);" data-value="10" data-abbr="TOR"><span><img src="assets/img/teams/png/10.png" class="team-img" alt="Toronto Maple Leafs" /></span>Toronto Maple Leafs</a>
-<a href="javascript:void(0);" data-value="59" data-abbr="UTA"><span><img src="assets/img/teams/png/59.png" class="team-img" alt="Utah Mammoth" /></span>Utah Mammoth</a>
-<a href="javascript:void(0);" data-value="23" data-abbr="VAN"><span><img src="assets/img/teams/png/23.png" class="team-img" alt="Vancouver Canucks" /></span>Vancouver Canucks</a>
-<a href="javascript:void(0);" data-value="54" data-abbr="VGK"><span><img src="assets/img/teams/png/54.png" class="team-img" alt="Vegas Golden Knights" /></span>Vegas Golden Knights</a>
-<a href="javascript:void(0);" data-value="15" data-abbr="WPG"><span><img src="assets/img/teams/png/15.png" class="team-img" alt="Washington Capitals" /></span>Washington Capitals</a>
-<a href="javascript:void(0);" data-value="52" data-abbr="WSH"><span><img src="assets/img/teams/png/52.png" class="team-img" alt="Winnipeg Jets" /></span>Winnipeg Jets</a>
+<a href="javascript:void(0);" data-value="24" data-abbr="ANA"><span><img src="assets/img/teams/png/24.png" class="team-img" alt="Anaheim Ducks" /></span><span class="sm-hide">Anaheim Ducks</span></a>
+<a href="javascript:void(0);" data-value="6" data-abbr="BOS"><span><img src="assets/img/teams/png/6.png" class="team-img" alt="Boston Bruins" /></span><span class="sm-hide">Boston Bruins</span></a>
+<a href="javascript:void(0);" data-value="7" data-abbr="BUF"><span><img src="assets/img/teams/png/7.png" class="team-img" alt="Buffalo Sabres" /></span><span class="sm-hide">Buffalo Sabres</span></a>
+<a href="javascript:void(0);" data-value="20" data-abbr="CGY"><span><img src="assets/img/teams/png/20.png" class="team-img" alt="Calgary Flames" /></span><span class="sm-hide">Calgary Flames</span></a>
+<a href="javascript:void(0);" data-value="12" data-abbr="CAR"><span><img src="assets/img/teams/png/12.png" class="team-img" alt="Carolina Hurricanes" /></span><span class="sm-hide">Carolina Hurricanes</span></a>
+<a href="javascript:void(0);" data-value="16" data-abbr="CHI"><span><img src="assets/img/teams/png/16.png" class="team-img" alt="Chicago Blackhawks" /></span><span class="sm-hide">Chicago Blackhawks</span></a>
+<a href="javascript:void(0);" data-value="21" data-abbr="COL"><span><img src="assets/img/teams/png/21.png" class="team-img" alt="Colorado Avalanche" /></span><span class="sm-hide">Colorado Avalanche</span></a>
+<a href="javascript:void(0);" data-value="29" data-abbr="CBJ"><span><img src="assets/img/teams/png/29.png" class="team-img" alt="Columbus Blue Jackets" /></span><span class="sm-hide">Columbus Blue Jackets</span></a>
+<a href="javascript:void(0);" data-value="25" data-abbr="DAL"><span><img src="assets/img/teams/png/25.png" class="team-img" alt="Dallas Stars" /></span><span class="sm-hide">Dallas Stars</span></a>
+<a href="javascript:void(0);" data-value="17" data-abbr="DET"><span><img src="assets/img/teams/png/17.png" class="team-img" alt="Detroit Red Wings" /></span><span class="sm-hide">Detroit Red Wings</span></a>
+<a href="javascript:void(0);" data-value="22" data-abbr="EDM"><span><img src="assets/img/teams/png/22.png" class="team-img" alt="Edmonton Oilers" /></span><span class="sm-hide">Edmonton Oilers</span></a>
+<a href="javascript:void(0);" data-value="13" data-abbr="FLA"><span><img src="assets/img/teams/png/13.png" class="team-img" alt="Florida Panthers" /></span><span class="sm-hide">Florida Panthers</span></a>
+<a href="javascript:void(0);" data-value="26" data-abbr="LAK"><span><img src="assets/img/teams/png/26.png" class="team-img" alt="Los Angeles Kings" /></span><span class="sm-hide">Los Angeles Kings</span></a>
+<a href="javascript:void(0);" data-value="30" data-abbr="MIN"><span><img src="assets/img/teams/png/30.png" class="team-img" alt="Minnesota Wild" /></span><span class="sm-hide">Minnesota Wild</span></a>
+<a href="javascript:void(0);" data-value="8" data-abbr="MTL"><span><img src="assets/img/teams/png/8.png" class="team-img" alt="Montréal Canadiens" /></span><span class="sm-hide">Montréal Canadiens</span></a>
+<a href="javascript:void(0);" data-value="18" data-abbr="NSH"><span><img src="assets/img/teams/png/18.png" class="team-img" alt="Nashville Predators" /></span><span class="sm-hide">Nashville Predators</span></a>
+<a href="javascript:void(0);" data-value="1" data-abbr="NJD"><span><img src="assets/img/teams/png/1.png" class="team-img" alt="New Jersey Devils" /></span><span class="sm-hide">New Jersey Devils</span></a>
+<a href="javascript:void(0);" data-value="2" data-abbr="NYI"><span><img src="assets/img/teams/png/2.png" class="team-img" alt="New York Islanders" /></span><span class="sm-hide">New York Islanders</span></a>
+<a href="javascript:void(0);" data-value="3" data-abbr="NYR"><span><img src="assets/img/teams/png/3.png" class="team-img" alt="New York Rangers" /></span><span class="sm-hide">New York Rangers</span></a>
+<a href="javascript:void(0);" data-value="9" data-abbr="OTT"><span><img src="assets/img/teams/png/9.png" class="team-img" alt="Ottawa Senators" /></span><span class="sm-hide">Ottawa Senators</span></a>
+<a href="javascript:void(0);" data-value="4" data-abbr="PHI"><span><img src="assets/img/teams/png/4.png" class="team-img" alt="Philadelphia Flyers" /></span><span class="sm-hide">Philadelphia Flyers</span></a>
+<a href="javascript:void(0);" data-value="5" data-abbr="PIT"><span><img src="assets/img/teams/png/5.png" class="team-img" alt="Pittsburgh Penguins" /></span><span class="sm-hide">Pittsburgh Penguins</span></a>
+<a href="javascript:void(0);" data-value="28" data-abbr="SJS"><span><img src="assets/img/teams/png/28.png" class="team-img" alt="San Jose Sharks" /></span><span class="sm-hide">San Jose Sharks</span></a>
+<a href="javascript:void(0);" data-value="55" data-abbr="SEA"><span><img src="assets/img/teams/png/55.png" class="team-img" alt="Seattle Kraken" /></span><span class="sm-hide">Seattle Kraken</span></a>
+<a href="javascript:void(0);" data-value="19" data-abbr="STL"><span><img src="assets/img/teams/png/19.png" class="team-img" alt="St. Louis Blues" /></span><span class="sm-hide">St. Louis Blues</span></a>
+<a href="javascript:void(0);" data-value="14" data-abbr="TBL"><span><img src="assets/img/teams/png/14.png" class="team-img" alt="Tampa Bay Lightning" /></span><span class="sm-hide">Tampa Bay Lightning</span></a>
+<a href="javascript:void(0);" data-value="10" data-abbr="TOR"><span><img src="assets/img/teams/png/10.png" class="team-img" alt="Toronto Maple Leafs" /></span><span class="sm-hide">Toronto Maple Leafs</span></a>
+<a href="javascript:void(0);" data-value="59" data-abbr="UTA"><span><img src="assets/img/teams/png/59.png" class="team-img" alt="Utah Mammoth" /></span><span class="sm-hide">Utah Mammoth</span></a>
+<a href="javascript:void(0);" data-value="23" data-abbr="VAN"><span><img src="assets/img/teams/png/23.png" class="team-img" alt="Vancouver Canucks" /></span><span class="sm-hide">Vancouver Canucks</span></a>
+<a href="javascript:void(0);" data-value="54" data-abbr="VGK"><span><img src="assets/img/teams/png/54.png" class="team-img" alt="Vegas Golden Knights" /></span><span class="sm-hide">Vegas Golden Knights</span></a>
+<a href="javascript:void(0);" data-value="15" data-abbr="WPG"><span><img src="assets/img/teams/png/15.png" class="team-img" alt="Washington Capitals" /></span><span class="sm-hide">Washington Capitals</span></a>
+<a href="javascript:void(0);" data-value="52" data-abbr="WSH"><span><img src="assets/img/teams/png/52.png" class="team-img" alt="Winnipeg Jets" /></span><span class="sm-hide">Winnipeg Jets</span></a>
