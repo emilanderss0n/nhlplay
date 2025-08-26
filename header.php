9a5e062684603324a139cf8797d12aafb8f95ebb
MoxoPixel <hello@emils.graphics>
2025-08-26 06:15:57 +0200

    Fix favicon paths in header.php for correct asset loading

--- header.php
+++ header.php
@@ -43,9 +43,9 @@ $mergedCSS = $isDevelopment ? false : generateMergedCSS([
     
     <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicon/favicon-192x192.png">
-    <link rel="icon" type="image/png" sizes="48x48" href="assets/img/favicon/favicon-48x48.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicon-192x192.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="assets/img/favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
 
     <?php if ($mergedCSS): ?>
         <link rel="stylesheet" type="text/css" media="screen" href="<?= $mergedCSS ?>">
