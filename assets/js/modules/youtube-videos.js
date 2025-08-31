510a2e66a4b0b2d701f52253a7e2c73efbd31812
MoxoPixel <hello@emils.graphics>
2025-08-31 08:23:22 +0200

    Refactor YouTube video element creation by removing redundant play button

--- assets/js/modules/youtube-videos.js
+++ assets/js/modules/youtube-videos.js
@@ -135,21 +135,12 @@ function createVideoElement(videoId, title, thumbnail, channelTitle) {
     videoDiv.innerHTML = `
         <div class="video-thumbnail-container">
             <img src="${thumbnail}" alt="${title}" class="video-thumbnail" loading="lazy">
-            <div class="play-button" data-video-id="${videoId}">
-
-                <svg fill="currentColor" width="44px" height="44px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m.001 1.165v21.669c.052.661.601 1.177 1.271 1.177.225 0 .436-.058.62-.16l-.006.003 21.442-10.8c.4-.192.671-.593.671-1.058s-.271-.867-.664-1.055l-.007-.003-21.442-10.8c-.177-.099-.388-.157-.613-.157-.672 0-1.223.521-1.27 1.181v.004z"/></svg>
-            </div>
         </div>
         <h4 class="video-title">${title}</h4>
     `;
     
     // Add click handler to open modal
-    const playButton = videoDiv.querySelector('.play-button');
     const thumbnailContainer = videoDiv.querySelector('.video-thumbnail-container');
-    
-    playButton.addEventListener('click', () => {
-        openVideoModal(videoId, channelTitle);
-    });
     
     // Also allow clicking the thumbnail image to open modal
     thumbnailContainer.addEventListener('click', () => {
