efc7d46068325ba119b2ba832d951f99bbcf96e6
MoxoPixel <hello@emils.graphics>
2025-09-03 06:33:01 +0200

    Rename "90's Edition (Theme)" back to "Phil Kessel Mode" in draft interface

--- assets/js/modules/draft-mode.js
+++ assets/js/modules/draft-mode.js
@@ -342,7 +342,7 @@ function createDraftInterface() {
                         <svg viewBox="0 0 64 64" height="2em" width="2em">
                             <path d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16" pathLength="575.0541381835938" class="path"></path>
                         </svg>
-                        <span>Phil Kessel Mode</span>
+                        <span>90's Edition (Theme)</span>
                     </label>
                     <button class="fancy-button start-draft-btn"><span><i class="bi bi-stars"></i> Start Draft</span></button>
                 </div>
